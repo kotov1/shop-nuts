0698e4c120b82a13c0a3e75c6785473153cee906
Petr Petukhov <kotov0991@gmail.com>
2017-12-15 20:11:31 +0300

    second section complete

--- app/card.php
+++ app/card.php
@@ -78,65 +78,113 @@
 		<div class="container">
 			<div class="row">
 				<div class="card__img" style="background-image: url('img/card/kedr2.jpg');"></div>
-				<div class="col-6 ml-auto">
+
+				<div class="col-lg-6 offset-lg-6">
 					<div class="card__heading">
 						<h3>Кедровый орех заботится о здоровье — это факт!</h3>
 					</div>
 				</div>
-				<div class="row">
 
-					<div class="col-lg-6">
-						<div class="vitamins">
-							<div class="row">
-								<div class="col-12"><h4>Витамины</h4></div>
-								<div class="col-lg-6">
-									<ul>
-										<li>PP 4,37  мг</li>
-										<li>A (РЭ) 1  мкг</li>
-										<li>B1 (тиамин) 0,364  мг</li>
-										<li>B2 (рибофлавин) 0,227  мг</li>
-										<li>B5 (пантотеновая) 0,313  мг</li>
-										<li>B6 (пиридоксин) 0,094  мг</li>
-									</ul>
-								</div>
-								<div class="col-lg-6">
-									<ul>
-										<li>B9 (фолиевая) 34  мкг</li>
-										<li>C 0,8  мг</li>
-										<li>E (ТЭ) 9,33  мг</li>
-										<li>К (филлохинон) 53,9  мкг</li>
-										<li>PP (Ниациновый эквивалент) 4,387  м</li>
-										<li>Бэта-каротин 0,017  мг</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="food-value">
-							<div class="row">
-								<div class="col-12"><h4>Пищевая ценность</h4></div>
-								<div class="col-lg-6">
-									<ul>
-										<li>Калорийность 673  кКал</li>
-										<li>Белки 13,69  гр</li>
-										<li>Жиры 68,37  гр</li>
-										<li>Углеводы 13,08  гр</li>
-										<li>Пищевые волокна 3,7  гр</li>
-									</ul>
-								</div>
-								<div class="col-lg-6">
-									<ul>
-										<li>Насыщеные жирные кислоты  4,899  гр</li>
-										<li></li>
-										<li></li>
-										<li></li>
-										<li></li>
-									</ul>
-								</div>
-							</div>
-						</div>
+				<!-- <div class="row"> -->
+
+					<div class="col-lg-6 col-12">
+						<div class="row">
+
+							<div class="col-lg-12 col-sm-7 col-12">
+								<div class="vitamins">
+									<div class="row">
+										<div class="col-12"><h4>Витамины</h4></div>
+										<div class="col-sm-6 col-6">
+											<ul>
+												<li>
+													<span class="vit-item">PP</span>
+													<span class="vit-descr">4,37  мг</span>
+												</li>
+												<li>
+													<span class="vit-item">A (РЭ)</span>
+													<span class="vit-descr">1  мкг</span>
+												</li>
+												<li>
+													<span class="vit-item">B1</span>
+													<span class="vit-descr">(тиамин) 0,364  мг</span>
+												</li>
+												<li>
+													<span class="vit-item">B2</span>
+													<span class="vit-descr">(рибофлавин) 0,227  мг</span>
+												</li>
+												<li>
+													<span class="vit-item">B5</span>
+													<span class="vit-descr">(пантотеновая) 0,313  мг</span>
+												</li>
+												<li>
+													<span class="vit-item">B6</span>
+													<span class="vit-descr">(пиридоксин) 0,094  мг</span>
+												</li>
+											</ul>
+										</div> <!-- /col-sm-6 col-6 -->
+
+										<div class="col-sm-6 col-6">
+											<ul>
+												<li>
+													<span class="vit-item">B9</span>
+													<span class="vit-descr">(фолиевая) 34  мкг</span>
+												</li>
+												<li>
+													<span class="vit-item">C</span>
+													<span class="vit-descr">0,8  мг</span>
+												</li>
+												<li>
+													<span class="vit-item">E</span>
+													<span class="vit-descr">(ТЭ) 9,33  мг</span>
+												</li>
+												<li>
+													<span class="vit-item">К</span>
+													<span class="vit-descr">(филлохинон) 53,9  мкг</span>
+												</li>
+												<li>
+													<span class="vit-item">PP</span>
+													<span class="vit-descr">(Ниациновый эквивалент) 4,387  м</span>
+												</li>
+												<li>
+													<span class="vit-item">БЭТА-КАРОТИН</span>
+													<span class="vit-descr">0,017  мг</span>
+												</li>
+											</ul>
+										</div> <!-- /.col-sm-6 col-6 -->
+									</div> <!-- /.row -->
+								</div> <!-- /.vitamins -->
+							</div> <!-- /.col-lg-12 col-sm-7 col-12 -->
+							
+							<div class="col-lg-12 col-sm-5 col-12">
+								<div class="food-value">
+									<div class="row">
+										<div class="col-12"><h4>Пищевая ценность</h4></div>
+										<div class="col-lg-6 col-12">
+											<ul>
+												<li>Калорийность 673  кКал</li>
+												<li>Белки 13,69  гр</li>
+												<li>Жиры 68,37  гр</li>
+												<li>Углеводы 13,08  гр</li>
+												<li>Пищевые волокна 3,7  гр</li>
+											</ul>
+										</div>
+										<div class="col-lg-6 col-12">
+											<ul class="to-top">
+												<li>Насыщеные жирные кислоты  4,899  гр</li>
+												<li>Крахмал 1,43  гр</li>
+												<li>Моно-и дисахариды 3,59  гр</li>
+												<li>Вода 2,28  гр</li>
+												<li>Зола 2,59  гр</li>
+											</ul>
+										</div>
+									</div> <!-- /.row -->
+								</div> <!-- /.food-value -->
+							</div> <!-- /.col-lg-12 col-sm-7 col-12 -->
+
+						</div> <!-- /.row -->
 					</div>
 
-					<div class="col-lg-6">
+					<div class="col-lg-6 col-12">
 						<p class="nut-history">Кедровые орехи или «семена долголетия» известны с XI века, но в России были обнаружены сибирскими исследователями в XV веке. Известен факт, что еще римские легионеры брали в поход горсть кедровых орехов, которые помогали набраться сил после тяжелых сражений.</p>
 						<div class="important-to-know">
 							<h4>Важно знать о Кедровом орехе:</h4>
@@ -149,10 +197,12 @@
 							</ul>
 						</div>
 					</div>
-				</div> <!-- /.row -->
+				<!-- </div> --> <!-- /.row -->
+
 				<div class="col-12">
 					<p class="energy-value">Энергетическая ценность Кедровый орех составляет 673 кКал.</p>
 				</div>
+
 			</div> <!-- /.row -->
 		</div> <!-- /.container -->
 	</section>
